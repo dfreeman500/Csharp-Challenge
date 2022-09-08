2bffaf882517dcfa06ba8e70647f2758579a4e66
Dan <danielfr500@gmail.com>
2022-09-07 23:51:16 -0400

    output using stringbuilder

--- README.md
+++ README.md
@@ -3,19 +3,3 @@
 
 
 An output challenge for a technical interview:
-I timed using string builder or using += for string outputs and String builder was much quicker 
-
-
-#
-The top output uses normal strings which are recreated with each += and this took 26 ms.
-
-
-
-
-
-
-
-The bottom ouput uses StringBuilder which was much quicker - 2ms. 
-
-
-<img src="images/timing.png" width="400" >
--- Technical-Interview/Program.cs
+++ Technical-Interview/Program.cs
@@ -1,26 +1,25 @@
-﻿string noMatch = "";
+﻿using System;
+using System.Text;
 
-for (int i = 99; i > 0; i--)
+public class NumberPairs
 {
-    string numberAsText = i.ToString();
+	public static void Main(string[] args)
+	{
+		StringBuilder noMatch = new StringBuilder("", 261);
+		for (int i = 99; i > 0; i--)
+		{
+			string numberAsText = Convert.ToString(i);
+			if (numberAsText.Length > 1 && Int32.Parse((numberAsText[0].ToString())) + Int32.Parse((numberAsText[1].ToString())) == 10)
+			{
+				Console.WriteLine($"Match found for {numberAsText}: ({numberAsText[0]},{numberAsText[1]})");
+			}
+			else
+			{
+				noMatch.Append($"{i},");
+			}
+		}
 
-    if (numberAsText.Length > 1 && Int32.Parse((numberAsText[0].ToString())) + Int32.Parse((numberAsText[1].ToString())) == 10)
-    {
-        Console.WriteLine($"Match found for {numberAsText}: ({numberAsText[0]},{numberAsText[1]})");
-    }
-    else
-    {
-        noMatch += numberAsText + ',';
-    }
+		Console.WriteLine("\nNo match found for:\n");
+		Console.WriteLine(noMatch.ToString(0, noMatch.Length - 1));
+	}
 }
-
-Console.WriteLine("\nNo match found for:\n");
-Console.WriteLine(noMatch.Substring(0, noMatch.Length - 1));
-
-//Don't need tryParse because I am limiting the input to ints between 99 and 1
-//short circuiting at the first expression in the if statement allows avoidance of System.IndexOutOfRangeException: 'Index was outside the bounds of the array.'
-
-//ToString().ToString()
-//difficult for me not to include the type in the variable name 
-
-//https://stackoverflow.com/questions/829174/is-there-an-easy-way-to-turn-an-int-into-an-array-of-ints-of-each-digit
